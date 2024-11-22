a549308410ecc02047eb785842cdd28296e45c0e
cenunix <user55596@protonmail.com>
2024-11-22 07:30:17 -0800

    astal update

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       packages.${system}.default = astal.lib.mkLuaPackage {
         inherit pkgs;
         name = "my-shell"; # how to name the executable
-        src = "./"; # should contain init.lua
+        src = ./astal; # should contain init.lua
 
         # add extra glib packages or binaries
         extraPackages = [
