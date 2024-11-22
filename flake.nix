c5e550cd72be93ae986e0a6e19f1cd9abecb9d6c
cenunix <user55596@protonmail.com>
2024-11-22 05:54:51 -0800

    astal update

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    astal = {
+      url = "github:aylur/astal";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, astal }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.${system}.default = astal.lib.mkLuaPackage {
+        inherit pkgs;
+        name = "my-shell"; # how to name the executable
+        src = ./init.lua; # should contain init.lua
+
+        # add extra glib packages or binaries
+        extraPackages = [ astal.packages.${system}.battery pkgs.dart-sass ];
+      };
+    };
+}
